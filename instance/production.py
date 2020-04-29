b07a5c28d05534ce6176534d6e9bd72828c1dbd2
Cédric Bonhomme <cedric@cedricbonhomme.org>
2020-04-29 10:53:24 +0200

    fix: [core] fixed some warnings.

--- instance/production.py
+++ instance/production.py
@@ -1,8 +1,8 @@
-HOST = '127.0.0.1'
+HOST = "127.0.0.1"
 PORT = 5000
 DEBUG = False
 TESTING = False
 
-MONGODB_HOST="localhost"
-MONGODB_PORT=27017
-MONGODB_DB="stats_api_dev"
+MONGODB_HOST = "localhost"
+MONGODB_PORT = 27017
+MONGODB_DB = "stats_api_dev"
