f111816a475415b24bec5edc52d54077dc3275ab
Cédric Bonhomme <cedric@cedricbonhomme.org>
2020-05-08 11:19:59 +0200

    new: [commands] Push stats for the organization specified in parameter to an other stats.

--- statsapi/commands.py
+++ statsapi/commands.py
@@ -1,17 +1,37 @@
 #! /usr/bin/env python
 # -*- coding: utf-8 -*-
 
+import json
 import click
+import requests
 
 from statsapi.bootstrap import application, db
 from statsapi.documents import Stats, Organization
 
 
 @application.cli.command("push-stats")
-@click.option('--uuid', default='', help='Organization UUID')
+@click.option("--uuid", default="", help="Organization UUID")
 def push_stats(uuid):
-    "Push stats."
+    """Push stats for the organization specified in parameter to an other stats
+    server.
+    """
     organization = Organization.objects.get(id__exact=uuid)
     stats = Stats.objects(organization__exact=organization)
     for stat in stats:
         print("{} {}".format(stat.created_at, stat.type))
+
+        # payload = stat.to_json()
+        payload = json.dumps(
+            {
+                "organization": uuid,
+                "type": stat.type,
+                "data": stat.data,
+                "day": stat.day,
+                "week": stat.week,
+                "month": stat.month,
+            }
+        )
+
+        r = requests.post(application.config["REMOTE_STATS_SERVER"], data=payload)
+
+        print(r.content)
