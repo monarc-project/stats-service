cdd7d3fbf2d7b2b120248a2917f097108301a1d8
Cédric Bonhomme <cedric@cedricbonhomme.org>
2020-05-12 23:55:26 +0200

    chg: [api] updated authentication to the API with the token X-API-KEY

--- statsapi/api/v1/common.py
+++ statsapi/api/v1/common.py
@@ -6,21 +6,16 @@ from statsapi.documents import Organization
 
 
 class ApiKeyAuthentication(AuthenticationBase):
-    """Custom token based authentication. To be inproved."""
-
+    """Custom token based authentication.
+    """
     def authorized(self):
         if not application.config["API_KEY_AUTHENTICATION"]:
             return True
-        if "AUTHORIZATION" in request.headers:
-            authorization = request.headers["AUTHORIZATION"].split()
-            if len(authorization) == 2 and authorization[0].lower() == "basic":
+        if "X-API-KEY" in request.headers:
+            token = request.headers.get("X-API-KEY", False)
+            if token:
                 try:
-                    token = authorization[1]
-                    token_key = Organization.objects.get(token__exact=token)
-                    print(token_key)
-                    # if token_key.user:
-                    #     login_user(token_key.user)
-                    #     setattr(current_user, 'token_key', token_key)
+                    organization = Organization.objects.get(token__exact=token)
                     return True
                 except (TypeError, UnicodeDecodeError, Organization.DoesNotExist):
                     pass
