6b48b6d73bf1f8d6e5314e6d671ab7d8d2db70b2
Cédric Bonhomme <cedric@cedricbonhomme.org>
2020-05-13 07:47:10 +0200

    chg: [documents] make token unique for organizations.

--- statsapi/documents/organization.py
+++ statsapi/documents/organization.py
@@ -3,7 +3,7 @@ from statsapi.bootstrap import db
 
 class Organization(db.Document):
     name = db.StringField(primary_key=True) # or maybe UUID
-    token = db.StringField(required=True)
+    token = db.StringField(required=True, unique=True)
 
     def __str__(self):
         return "Name: {}\nToken: {}".format(self.name, self.token)
