96297e0c67915ac23b8985536332ce0eda918705
Cédric Bonhomme <cedric@cedricbonhomme.org>
2020-05-15 10:16:57 +0200

    chg: [core] reformat with black and added a new fiel for the marshalling of stats object for api v2

--- statsservice/api/v2/stats.py
+++ statsservice/api/v2/stats.py
@@ -4,56 +4,65 @@ from flask_restx import Api, Resource, fields, reqparse
 from statsservice.documents import Stats, Organization
 
 
-blueprint = Blueprint('api', __name__, url_prefix='/api/v2/stats')
-api = Api(blueprint,
-    title='MONARC Stats service - API v2',
-    version='2.0',
-    description='API v2 of the MONARC Stats service',
-    doc='/swagger/',
+blueprint = Blueprint("api", __name__, url_prefix="/api/v2/stats")
+api = Api(
+    blueprint,
+    title="MONARC Stats service - API v2",
+    version="2.0",
+    description="API v2 of the MONARC Stats service",
+    doc="/swagger/",
     # All API metadatas
 )
 
 
 # Argument Parsing
 parser = reqparse.RequestParser()
-parser.add_argument('organization', type=str, help='Organization of the stats')
-parser.add_argument('anr', type=str, help='Anr of the stats')
-parser.add_argument('type', type=str, help='Type of the stats')
-parser.add_argument('day', type=int, help='Type of the stats')
-parser.add_argument('month', type=int, help='Type of the stats')
-parser.add_argument('year', type=int, help='Type of the stats')
-parser.add_argument('page', type=int, default=1, location='args')
-parser.add_argument('per_page', type=int, location='args')
+parser.add_argument("organization", type=str, help="Organization of the stats")
+parser.add_argument("anr", type=str, help="Anr of the stats")
+parser.add_argument("type", type=str, help="Type of the stats")
+parser.add_argument("day", type=int, help="Type of the stats")
+parser.add_argument("month", type=int, help="Type of the stats")
+parser.add_argument("year", type=int, help="Type of the stats")
+parser.add_argument("page", type=int, default=1, location="args")
+parser.add_argument("per_page", type=int, location="args")
 
 
 # Response marshalling
-stats = api.model('Stats', {
-    'uuid': fields.String(readonly=True, description='The stats unique identifier'),
-    'organization.name': fields.String(readonly=True, description='The stats organization'),
-    'anr': fields.String(),
-    'type': fields.String(),
-    'day': fields.Integer(),
-    'week': fields.Integer(),
-    'month': fields.Integer(),
-    'data': fields.Raw(),
-    'created_at': fields.DateTime(),
-    'updated_at': fields.DateTime(),
-})
+stats = api.model(
+    "Stats",
+    {
+        "uuid": fields.String(readonly=True, description="The stats unique identifier"),
+        "organization": fields.String(
+            readonly=True,
+            attribute=lambda x: x.organization.name,
+            description="The stats organization",
+        ),
+        "anr": fields.String(),
+        "type": fields.String(),
+        "day": fields.Integer(),
+        "week": fields.Integer(),
+        "month": fields.Integer(),
+        "year": fields.Integer(),
+        "data": fields.Raw(),
+        "created_at": fields.DateTime(),
+        "updated_at": fields.DateTime(),
+    },
+)
 
-stats_list_fields = api.model('StatsList', {
-    'metadata': fields.Raw(),
-    'data': fields.List(fields.Nested(stats))
-})
+stats_list_fields = api.model(
+    "StatsList", {"metadata": fields.Raw(), "data": fields.List(fields.Nested(stats))}
+)
 
 
-@api.route('/')
+@api.route("/")
 class StatsList(Resource):
-    '''Shows a list of all stats, and lets you POST to add new stats'''
-    @api.doc('list_stats')
+    """Shows a list of all stats, and lets you POST to add new stats"""
+
+    @api.doc("list_stats")
     @api.expect(parser)
     @api.marshal_list_with(stats_list_fields, skip_none=True)
     def get(self):
-        '''List all stats'''
+        """List all stats"""
         args = parser.parse_args()
         args = {k: v for k, v in args.items() if v is not None}
 
@@ -62,12 +71,7 @@ class StatsList(Resource):
 
         result = {
             "data": [{}],
-            "metadata": {
-                "total": 0,
-                "count": 0,
-                "page": page,
-                "per_page": per_page,
-            },
+            "metadata": {"total": 0, "count": 0, "page": page, "per_page": per_page,},
         }
 
         try:
@@ -86,38 +90,38 @@ class StatsList(Resource):
 
         return result, 200
 
-    @api.doc('create_stats')
+    @api.doc("create_stats")
     @api.expect(stats)
     @api.marshal_with(stats, code=201)
     def post(self):
-        '''Create a new stats'''
+        """Create a new stats"""
         print(api.payload)
         new_stat = Stats(**api.payload)
         return new_stat.save(), 201
 
 
-
-@api.route('/<string:uuid>')
-@api.response(404, 'Stats not found')
-@api.param('uuid', 'The stats identifier')
+@api.route("/<string:uuid>")
+@api.response(404, "Stats not found")
+@api.param("uuid", "The stats identifier")
 class StatsItem(Resource):
-    '''Show a single stats item and lets you delete them'''
-    @api.doc('get_stats')
+    """Show a single stats item and lets you delete them"""
+
+    @api.doc("get_stats")
     @api.marshal_with(stats)
     def get(self, uuid):
-        '''Fetch a given resource'''
+        """Fetch a given resource"""
         return Stats.objects.get(uuid__exact=uuid), 200
 
-    @api.doc('delete_stats')
-    @api.response(204, 'Stats deleted')
+    @api.doc("delete_stats")
+    @api.response(204, "Stats deleted")
     def delete(self, uuid):
-        '''Delete a stats given its identifier'''
-        #DAO.delete(id)
-        return '', 204
+        """Delete a stats given its identifier"""
+        # DAO.delete(id)
+        return "", 204
 
     @api.expect(stats)
     @api.marshal_with(stats)
     def put(self, uuid):
-        '''Update a stats given its identifier'''
-        #return DAO.update(id, api.payload)
+        """Update a stats given its identifier"""
+        # return DAO.update(id, api.payload)
         pass
