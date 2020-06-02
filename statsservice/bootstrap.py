e1d85cbbed62e288c76e287812282fe2db443d4f
Cédric Bonhomme <cedric@cedricbonhomme.org>
2020-06-02 15:59:02 +0200

    style change

--- statsservice/bootstrap.py
+++ statsservice/bootstrap.py
@@ -14,7 +14,9 @@ application = Flask(__name__, instance_relative_config=True)
 
 if ON_HEROKU:
     application.config.from_pyfile("heroku.py", silent=False)
-    application.config["MONGODB_HOST"] = os.environ.get("MONGODB_URI", "") + "?retryWrites=false"
+    application.config["MONGODB_HOST"] = (
+        os.environ.get("MONGODB_URI", "") + "?retryWrites=false"
+    )
     application.config["INSTANCE_URL"] = os.environ.get("INSTANCE_URL", "")
 else:
     application.config.from_pyfile("production.py", silent=False)
