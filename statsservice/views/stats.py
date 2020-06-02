522566c989b984b5986c19af3abde881c2cd4ee7
Cédric Bonhomme <cedric@cedricbonhomme.org>
2020-06-02 13:42:09 +0200

    chg: [stats/risks] removed useless import

--- statsservice/views/stats.py
+++ statsservice/views/stats.py
@@ -20,7 +20,7 @@
 # You should have received a copy of the GNU Affero General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-from flask import Blueprint, render_template, jsonify
+from flask import Blueprint
 
 from statsservice.documents import Stats
 
