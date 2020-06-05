5e754c5ada56bbb7e9a814a478acf462b048e538
Cédric Bonhomme <cedric@cedricbonhomme.org>
2020-06-05 10:35:16 +0200

    new: [documents] Added created_at and updated_at properties for organizations.

--- statsservice/documents/organization.py
+++ statsservice/documents/organization.py
@@ -1,4 +1,5 @@
 import secrets
+import datetime
 
 from statsservice.bootstrap import db
 
@@ -6,6 +7,8 @@ from statsservice.bootstrap import db
 class Organization(db.Document):
     name = db.StringField(primary_key=True)  # or maybe UUID
     token = db.StringField(required=True, unique=True, default=secrets.token_urlsafe(64))
+    created_at = db.DateTimeField(default=datetime.datetime.utcnow)
+    updated_at = db.DateTimeField(default=datetime.datetime.utcnow)
 
     def __str__(self):
-        return "Name: {}\nToken: {}".format(self.name, self.token)
+        return "Name: {}\nToken: {}\nCreated at:{}".format(self.name, self.token, self.created_at)
