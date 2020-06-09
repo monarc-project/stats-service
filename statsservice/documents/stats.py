e64cd73e1f5910cf55f33af09389a022b669ac62
Cédric Bonhomme <cedric@cedricbonhomme.org>
2020-06-09 16:00:37 +0200

    chg: [documents] Pre save data validation and cleaning.

--- statsservice/documents/stats.py
+++ statsservice/documents/stats.py
@@ -1,7 +1,9 @@
 import uuid
 import datetime
+from mongoengine import ValidationError
 
 from statsservice.bootstrap import db
+from statsservice.lib.validation import validate_data
 from statsservice.documents import Organization
 
 
@@ -18,6 +20,17 @@ class Stats(db.Document):
     data = db.DynamicField(required=True)
     created_at = db.DateTimeField(default=datetime.datetime.utcnow)
     updated_at = db.DateTimeField(default=datetime.datetime.utcnow)
+
+
+    @staticmethod
+    def data_validation(data, type):
+        if not validate_data(data, type):
+            raise ValidationError("data not valid for type {}".format(type))
+
+
+    def clean(self):
+        self.data_validation(self.data, self.type)
+
 
     def __str__(self):
         return """Organization: {}
