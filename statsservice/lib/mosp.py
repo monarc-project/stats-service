b358cfe33792e5792c11b776c0a4b5af0ccd9c49
Cédric Bonhomme <cedric@cedricbonhomme.org>
2020-06-25 11:31:55 +0200

    new: [lib] Added bridge module with a MOSP platform.

---
+++ statsservice/lib/mosp.py
@@ -0,0 +1,13 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+
+#
+# Bridge with a MOSP platform. By default: https://objects.monarc.lu
+#
+
+import requests
+
+
+def is_objects_published(stats):
+    """Doc."""
+    pass
