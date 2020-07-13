2244def2a582c91484ebe93476f4c221530f35e2
Cédric Bonhomme <cedric@cedricbonhomme.org>
2020-07-13 11:32:40 +0200

    chg: [style] reformat

--- statsservice/lib/stats.py
+++ statsservice/lib/stats.py
@@ -23,9 +23,11 @@ def average_threats(threats):
     frames = defaultdict(list)
 
     for anr_uuid in grouped_threats:
+        print("Averages for ANR (for threats): {}".format(anr_uuid))
         for threat_uuid, stats in grouped_threats[anr_uuid].items():
             frames[threat_uuid].append(stats)
             df = pd.DataFrame(stats)
             result = dict(df.mean())
-            if threat_uuid == 'b402d4e0-4576-11e9-9173-0800277f0571':
-                print(result)
+            if True:#threat_uuid == 'b402d4e0-4576-11e9-9173-0800277f0571':
+                print("{} : {}".format(threat_uuid, result))
+        print()
