42b91b966f001ce7a56a401c0476958c30166aac
Cédric Bonhomme <cedric@cedricbonhomme.org>
2020-07-23 09:47:49 +0200

    chg: [commands] add the possibility to specify UUID when creating client (since the name will probably be removed)

--- statsservice/commands/client.py
+++ statsservice/commands/client.py
@@ -1,4 +1,5 @@
 import click
+import uuid
 import secrets
 
 from statsservice.bootstrap import application, db
@@ -6,20 +7,30 @@ from statsservice.models import Client
 
 
 @application.cli.command("create_client")
-@click.option("--name", required=True, help="Client name (or UUID)")
+@click.option("--name", required=True, help="Client name")
+@click.option("--uuid", default="", help="Client UUID")
 @click.option(
     "--role",
     default="user",
     help="Role of the client (user or admin)",
     show_default=True,
 )
-def create_client(name, role):
+def create_client(name, uuid, role):
     """Create an client.
     """
+    args = {}
     token = secrets.token_urlsafe(64)
-    new_client = Client(
-        name=name, token=token, is_active=True, role=(1 if role == "user" else 2)
+    if uuid:
+        args["uuid"] = uuid
+    args.update(
+        {
+            "name": name,
+            "token": token,
+            "is_active": True,
+            "role": (1 if role == "user" else 2),
+        }
     )
+    new_client = Client(**args)
     db.session.add(new_client)
     db.session.commit()
     print(new_client)
