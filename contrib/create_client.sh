63b504f8fa7810ffbb1ad97c2336fe1b0049749c
Cédric Bonhomme <cedric@cedricbonhomme.org>
2020-11-18 00:01:33 +0100

    export conf variable for flask cli

--- contrib/create_client.sh
+++ contrib/create_client.sh
@@ -3,4 +3,7 @@
 
 /home/ansible/.poetry/bin/poetry env use /home/ansible/.pyenv/shims/python
 
+export FLASK_APP=runserver.py
+export STATS_CONFIG=production.py
+
 /home/ansible/.poetry/bin/poetry run flask client_create --name $1 --token $2
