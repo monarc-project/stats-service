29c28b9734f6a105edd138553638a0ade7ebeea3
Cédric Bonhomme <cedric@cedricbonhomme.org>
2020-11-18 08:06:32 +0100

    chg: [contrib] comment the lines which set configuration variables.

--- contrib/create_client.sh
+++ contrib/create_client.sh
@@ -1,9 +1,16 @@
 #! /usr/bin/env bash
 
+if [ $# -lt 3 ]
+  then
+    echo "Two arguments are required. Usage:"
+    echo "./create_client.sh CLIENT_NAME CLIENT_TOKEN"
+    exit 1
+fi
 
-/home/ansible/.poetry/bin/poetry env use /home/ansible/.pyenv/shims/python
+#export FLASK_APP=runserver.py
+#export STATS_CONFIG=production.py
 
-export FLASK_APP=runserver.py
-export STATS_CONFIG=production.py
+poetry env use /home/ansible/.pyenv/shims/python
 
-/home/ansible/.poetry/bin/poetry run flask client_create --name $1 --token $2
+
+poetry run flask client_create --name $1 --token $2
