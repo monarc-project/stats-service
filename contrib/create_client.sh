77b7d5396b709742c8ecd440faecd50032c51ed5
Cédric Bonhomme <cedric@cedricbonhomme.org>
2020-11-18 11:17:53 +0100

    chg: [contrib] restored conf variable when creating new client

--- contrib/create_client.sh
+++ contrib/create_client.sh
@@ -7,10 +7,9 @@ if [ ! $# -eq 2 ]
     exit 1
 fi
 
+/home/ansible/.poetry/bin/poetry env use /home/ansible/.pyenv/shims/python
+
 export FLASK_APP=runserver.py
 export STATS_CONFIG=production.py
 
-poetry env use /home/ansible/.pyenv/shims/python
-
-
-poetry run flask client_create --name $1 --token $2
+/home/ansible/.poetry/bin/poetry run flask client_create --name $1 --token $2
