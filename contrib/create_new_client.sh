edcbaee63524c16c1ca43e22536b04a55698ebef
Ruslan Baidan <ruslan.baidan@securitymadein.lu>
2021-01-25 13:52:29 +0100

    Create create_new_client.sh
    Added the script to create a new client from cli.

---
+++ contrib/create_new_client.sh
@@ -0,0 +1,18 @@
+#! /usr/bin/env bash
+
+if [ ! $# -ge 1 ]
+  then
+    echo "CLIENT_NAME argument is required, CLIENT_TOKEN is optional. Usage:"
+    echo "./create_client.sh CLIENT_NAME CLIENT_TOKEN"
+    exit 1
+fi
+
+export FLASK_APP=runserver.py
+export STATS_CONFIG=production.py
+
+if [ -z "$2" ]
+  then
+    poetry run flask client_create --name $1
+  else
+    poetry run flask client_create --name $1 --token $2
+fi
