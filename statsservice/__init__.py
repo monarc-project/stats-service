23df33092e78f330fd59908d2120bdb538dc7fdf
Cédric Bonhomme <cedric@cedricbonhomme.org>
2021-02-18 12:17:01 +0100

    fix: [core] handle case distribution is not installed in case of deployment via pypi

--- statsservice/__init__.py
+++ statsservice/__init__.py
@@ -21,7 +21,10 @@ def get_version():
         .strip()
     ) or ""
     if not version:
-        version = pkg_resources.get_distribution("statsservice").version
+        try:
+            version = pkg_resources.get_distribution("statsservice").version
+        except pkg_resources.DistributionNotFound:
+            version = ""
     return version
 
 
