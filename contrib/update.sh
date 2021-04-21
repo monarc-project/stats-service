fc5a05d0b52512961c707aa223b512d6f4bff472
Cédric Bonhomme <cedric@cedricbonhomme.org>
2021-04-21 18:06:16 +0200

    chg: improved updated script.

--- contrib/update.sh
+++ contrib/update.sh
@@ -4,9 +4,22 @@
 # Update Stats Service.
 #
 
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+NC='\033[0m' # No Color
+
+set -e
+#set -x
+
+export FLASK_APP=runserver.py
+
 git pull origin master --tags
 npm install
 poetry install --no-dev
 poetry run pybabel compile -d statsservice/translations
 poetry run flask db upgrade
-sudo systemctl restart statsservice.service
+
+echo -e "✨ 🌟 ✨"
+echo -e "${GREEN}Stats Service updated. You can now restart the service.${NC} Examples:"
+echo "    sudo systemctl restart statsservice.service"
+echo "    sudo systemctl restart apache2.service"
