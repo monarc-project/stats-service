ec8eb96205a59d4ec94d2aedfa2793873d33cf75
Cédric Bonhomme <cedric@cedricbonhomme.org>
2021-04-28 13:45:05 +0200

    chg: [views] Updated clients.json endpoint.

--- statsservice/views/map.py
+++ statsservice/views/map.py
@@ -3,7 +3,8 @@
 
 from flask import Blueprint, render_template, request, jsonify, abort
 
-from statsservice.models import Client
+import statsservice.lib.processors
+from statsservice.models import Client, Stats
 
 
 # stats_bp: blueprint for public only routes which returns different kind of statistics
@@ -17,11 +18,18 @@ def index():
 
 @map_bp.route("/clients.json", methods=["GET"])
 def clients():
-    clients = Client.query.filter().all()
+    clients = Client.query.filter(Client.latitude != None, Client.longitude != None).all()
 
     result = []
 
     for client in clients:
+
+        query = Stats.query.filter(Stats.type == "threat", Stats.client_id == client.id)
+        query = query.order_by(Stats.date.desc()).limit(40)
+        threats = getattr(statsservice.lib.processors, "threat_average_on_date")(query.all())
+        for threat in threats:
+            print(threat)
+
         result.append({
             "latitude": client.latitude,
             "longitude": client.longitude,
